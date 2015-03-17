25143d7c2b9868d36b90eebe62a1225bc2eddee8
guspowell <augustuspowell@me.com>
2015-03-17 11:32:07 +0000

    can add items and quantity to a new table order in the browser

--- server.rb
+++ server.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require './lib/shop.rb'
+require './lib/order.rb'
+
 
 class TillApp < Sinatra::Base
   get '/' do
@@ -7,15 +9,25 @@ class TillApp < Sinatra::Base
   end
 
   shop = Shop.new
+  shop.load_information
 
   get '/index' do
-    shop.load_information
     @name = shop.name
     erb :index
   end
 
   get '/mainpage' do
+    @name = shop.name
+    @time = Time.new.strftime("%B %e, %Y")
+    @order = Order.new(1)
     erb :mainpage
+  end
+
+  post '/mainpage' do
+    table_number, item, quantity = params[:table_number].to_i, params[:item], params[:quantity].to_i
+    @order = Order.new(table_number)
+    @order.add_item(item, quantity)
+    redirect to('/mainpage')
   end
 
   # start the server if ruby file executed directly
